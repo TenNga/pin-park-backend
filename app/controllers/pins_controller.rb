07cf9c794a885db5c612de33a4622515137ef693
kevin <tenngawang6080@gmail.com>
2020-06-16 00:37:29 -0400

    routes done and association works

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,2 +1,14 @@
 class PinsController < ApplicationController
+    def index
+        pins = Pin.all 
+
+        render json: pins
+    end
+
+    def show
+        pin = Pin.find(params[:id])
+
+        render json: pin
+    end
+
 end
