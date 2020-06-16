2baf2e3e4fa0afcc8c065d52412633d69d1dc02f
kevin <tenngawang6080@gmail.com>
2020-06-15 23:50:56 -0400

    user routes setup done with test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = [
+    {
+        "id": 1,
+        "name": "kevin",
+        "email": "kk@gmail.com",
+        "phone_number": 2343332233
+    },
+    {
+        "id": 2,
+        "name": "karma",
+        "email": "karma@gmail.com",
+        "phone_number": 4454342999
+    },
+    {
+        "id": 3,
+        "name": "ngawang",
+        "email": "ngawang@gmail.com",
+        "phone_number": 3346679292
+    },
+]
+
+users.each do |user| 
+    User.create(user)
+end
+
